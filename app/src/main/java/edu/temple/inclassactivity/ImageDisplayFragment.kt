87365032aa35741cef5e35b335cbc49af8a19c95
Jenny Ni <tun48961@temple.edu>
2023-03-23 12:18:45 -0400

    added ViewModal code

--- app/src/main/java/edu/temple/inclassactivity/ImageDisplayFragment.kt
+++ app/src/main/java/edu/temple/inclassactivity/ImageDisplayFragment.kt
@@ -9,16 +9,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 const val IMAGES_KEY = "imageList"
 
 class ImageDisplayFragment : Fragment() {
-
     private lateinit var images: IntArray
+    private val mainViewModel:MainViewModel by lazy {
+        ViewModelProvider(requireActivity())[MainViewModel::class.java]
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // If we have arguments
         arguments?.let { it ->
@@ -27,7 +30,7 @@ class ImageDisplayFragment : Fragment() {
                 images = it
             }
         }
-    }
+    }*/
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // The inflated layout file is returned to the parent/host and displayed to the user
@@ -40,17 +43,20 @@ class ImageDisplayFragment : Fragment() {
         // The recycler view is the root element of the Fragment's layout
         // as such the view argument passed to onViewCreated() is the RecyclerView
         with (view as RecyclerView) {
-            adapter = CustomRecyclerAdapter(images)
+            mainViewModel.getImageIds().observe(requireActivity()){
+                adapter = CustomRecyclerAdapter(it)
+            }
+
             layoutManager = GridLayoutManager(requireContext(), 2)
         }
     }
 
-    companion object {
+    /*companion object {
         fun newInstance(images: IntArray) =
             ImageDisplayFragment().apply {
                 arguments = Bundle().apply {
                     putIntArray(IMAGES_KEY, images)
                 }
             }
-    }
+    }*/
 }
